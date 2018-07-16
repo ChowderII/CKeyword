fe24858890349509098bdb2cec9074cd243a5743
ChowderII <aboye018@uottawa.ca>
2018-07-16 11:19:17 -0400

    New test

--- test/test.c
+++ test/test.c
@@ -5,8 +5,14 @@
 /* entry function to the threads */
 void *entry(void *x_void_ptr)
 {
-	sleep(1);
-	printf("Hello World %d\n", x_void_ptr);
+	if (x_void_ptr != -1) {
+		sleep(1);
+		printf("Spawning thread from inside the thread %d\n", x_void_ptr);
+		pthread_t myThreadID2;
+		printf("Before Thread\n");
+		pthread_create(&myThreadID2, NULL, entry, -1);
+		pthread_join(myThreadID2, NULL);
+	}
 	return NULL;
 }
 
@@ -14,8 +20,8 @@ int main()
 {
 	pthread_t myThreadID;
 	printf("Before Thread\n");
-	pthread_create(&myThreadID, NULL, entry, 2);
+	pthread_create(&myThreadID, NULL, entry, 1234);
 	pthread_join(myThreadID, NULL);
-	printf("After Thread\m");
+	printf("After Thread\n");
 	exit(0);
 }
