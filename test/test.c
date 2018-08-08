68bda5f55ef83e3895266f91f68263d4f57ec2b7
Alexandre Boyer <aboye018@uottawa.ca>
2018-08-08 21:01:29 +0000

    continued testing on pointers

--- test/test.c
+++ test/test.c
@@ -5,8 +5,10 @@
 #include "include/tokenize.h"
 
 int main(void) {
-    Word *(*pTokens) = NULL;
-    pTokens = malloc(sizeof(Word*));
-    Word test = {.word = "TEST"};
-    *pTokens[0] = &test;
+    Word *(*pTokens) = NULL;  // empty pointer of pointer to Word struct
+    pTokens = malloc(sizeof(Word*)*3);
+    pTokens[0]->word = malloc(strlen("TEST"));
+    pTokens[0]->word = "TEST";
+    printf("%s", pTokens[0]->word);
+    return 0;
 }
